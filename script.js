7a90dc34e785785915d524eee49444add3b5e873
AndreaC05 <131610224+AndreaC05@users.noreply.github.com>
2023-05-07 17:10:59 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
 let menuVisible = false;
-//funcion que oculta o muestra menu
-function mostrarOcultarMenu(){
+
+// Funcion que oculta o muestra menu
+
+function mostrarOcultarMenu() {
+
+	let navbar = document.getElementById('nav');
+
 	if (menuVisible) {
-		document.getElementById("nav").classList ="";
+
+		navbar.classList.remove('responsive');
 		menuVisible = false;
-	}else{
-		document.getElementById("nav").classList ="responsive";
+
+	} else {
+
+		navbar.classList.add('responsive');
 		menuVisible = true;
+
 	}
 }
 
 function seleccionar(){
+
 	//oculto el menu una vez que selecciono una opcion
 	document.getElementById("nav").classList ="";
 	menuVisible = false;
+
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	const elementosCarousel = document.querySelectorAll('.carousel');
+
+	M.Carousel.init(elementosCarousel, {
+
+		duration: 150,
+		dist: -80,
+		shift: 5,
+		padding: 6,
+		numVisible: 4,
+		indicators: true,
+		noWrap: false
+
+	});
+
+});
